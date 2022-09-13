4a31ec16969d36076ffe424bf9bf09ce3601d89b
Yuta <y.sugiyama54@gmail.com>
2022-09-13 23:57:12 +0900

    第3章 Template Method

---
+++ src/main/java/com/example/design/AbstractDisplay.java
@@ -0,0 +1,18 @@
+package com.example.design;
+
+public abstract class AbstractDisplay {
+
+    // open, print, closeはサブクラスに実装を任せる抽象メソッド
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+
+    // displayはAbstractDisplayで実装しているメソッド
+    public final void display() {
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+}
---
+++ src/main/java/com/example/design/CharDisplay.java
@@ -0,0 +1,26 @@
+package com.example.design;
+
+public class CharDisplay extends AbstractDisplay {
+
+    private char ch;
+
+    // コンストラクタ
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+
+    @Override
+    public void open() {
+        System.out.print("<<");
+    }
+
+    @Override
+    public void print() {
+        System.out.print(ch);
+    }
+
+    @Override
+    public void close() {
+        System.out.println(">>");
+    }
+}
