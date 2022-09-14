fbfac60a5a685a8bf237727e28e7297f823c071b
Yuta <y.sugiyama54@gmail.com>
2022-09-14 23:48:23 +0900

    第4章 Factory Method

--- src/main/java/com/example/design/Main.java
+++ src/main/java/com/example/design/Main.java
@@ -1,6 +1,17 @@
 package com.example.design;
 
+import com.example.design.framework.Factory;
+import com.example.design.framework.Product;
+import com.example.design.idcard.IDCardFactory;
+
 public class Main {
     public static void main(String[] args) {
+        Factory factory = new IDCardFactory();
+        Product card1 = factory.create("Sugiyama Yuta");
+        Product card2 = factory.create("Kishida Fumio");
+        Product card3 = factory.create("Suga Yoshihide");
+        card1.use();
+        card2.use();
+        card3.use();
     }
 }
