c6cb8423c0dfcff2b7bcf323c1becdc7099f3911
Yuta <y.sugiyama54@gmail.com>
2022-09-15 22:59:54 +0900

    第5章 Singleton

--- src/main/java/com/example/design/Main.java
+++ src/main/java/com/example/design/Main.java
@@ -2,5 +2,14 @@ package com.example.design;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Start");
+        Singleton obj1 = Singleton.getSingleton();
+        Singleton obj2 = Singleton.getSingleton();
+        if (obj1 == obj2) {
+            System.out.println("obj1とonj2は同じインスタンスです。");
+        } else {
+            System.out.println("obj1とonj2は同じインスタンスではありません。");
+        }
+        System.out.println("End");
     }
 }
